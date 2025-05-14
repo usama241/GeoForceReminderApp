e5cc6642be225b8c67eb03dcf31c70f4991fa44c
usama aslam <usama.aslam@appinsnap.com>
2025-05-14 11:20:37 +0500

    code cleaning before coreData implementation.

--- GeoForceReminderApp/StartUp/AppDelegate.swift
+++ GeoForceReminderApp/StartUp/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var redirect : RedirectHelper!
     var logoutTimer: Timer?
-    let vc = BaseViewController()
+//    let vc = BaseViewController()
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
